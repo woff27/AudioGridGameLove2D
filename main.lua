db795500300d2f475cb1be1d40678be3b1125cb6
Jay Fernandes <woff27@gmail.com>
2014-03-25 10:50:50 -0700

    working animations

--- main.lua
+++ main.lua
@@ -73,33 +73,43 @@ function love.update(dt)
 	x_Coord = (((love.graphics.getWidth()/gridSize) - Board_x)/2)*gridSize
 	y_Coord = (((love.graphics.getHeight()/gridSize) - Board_y)/2)*gridSize
 	--t = speed of animation, playcol = which column is being animated (ex: playcol+2 skips columns)
+	if startbtn > 0 then
 	t = t + dt/2
 	if t > 15/bpm then 
 		t = 0
-		playcolR = (playcolR + 1) % colEndR
-		playcolU = (playcolU + 1) % colEndU
-		playcolL = (playcolL + 1) % colEndL
-		playcolD = (playcolD + 1) % colEndD
-		if playcolR == 0 then
+		playcolR = (playcolR + 1) --% colEndR
+		playcolD = (playcolD + 1) --% colEndD
+		playcolU = (playcolU - 1) --% colEndU
+		playcolL = (playcolL - 1) --% colEndL
+		if playcolR == colEndR then
 			playcolR = 1
-			if startbtn >= 1 then
+			if playcolR == 1 then
 				love.audio.play(rSound) 
 			end
 		end
-		if playcolU == 0 then playcolU = 1 end
-		if playcolL == 0 then playcolL = 1 end
-		if playcolD == 0 then 
-			playcolD = 1
-			if startbtn >= 1 then
+		if playcolU == -colEndU then 
+			playcolU= 1 
+			if playcolU == 1 then
+				love.audio.play(rSound) 
+			end
+		end
+		if playcolL == -colEndL then 
+			playcolL = 1
+			if playcolL == 1 then
 				love.audio.play(dSound)
 			end
 		end
+		if playcolD == colEndD then 
+			playcolD = 1
+			if playcolD == 1 then
+				love.audio.play(dSound)
+			end
+		end
+	end
 	end 
 end
 
 function love.draw()
-	CreateBoard(8,8)
-
     if love.mouse.isDown("l") then
 		mPosX, mPosY = love.mouse.getPosition()    
 		MouseClick()	
@@ -111,10 +121,10 @@ function love.draw()
 				if mPosX > v.x and mPosX < v.x + gridSize and mPosY > v.y and mPosY < v.y + gridSize then
 					drawX = v.x
 					drawY = v.y
-					colEndR = Board_x-((v.x-x_Coord)/gridSize)+1
-					colEndU = Board_y-((v.y-y_Coord)/gridSize)+1
-					colEndL = Board_x-((v.x-x_Coord)/gridSize)+1
+					colEndR = Board_x-((v.x-x_Coord)/gridSize)+1 --ex: 8-((300-200)/50)+1 = 5 
 					colEndD = Board_y-((v.y-y_Coord)/gridSize)+1
+					colEndL = Board_x-(Board_x-((v.x-x_Coord)/gridSize))
+					colEndU = Board_y-(Board_y-((v.y-y_Coord)/gridSize))
 
 					v.startCnt = 1
 					startbtn = 1
@@ -122,34 +132,63 @@ function love.draw()
 			end
 		end
 	end
+CreateBoard(8,8)
+
+	if startbtn > 0 then
+		if playcolL == 1 then
+	    	love.graphics.setColor(0, Pulse+150, Pulse+25, Pulse)
+	   		love.graphics.rectangle('fill', 0, 0, x_Coord, 600) 
+	 	end
+	 	if playcolU == 1 then
+	    	love.graphics.setColor(0, Pulse+150, Pulse+25, Pulse)
+	   		love.graphics.rectangle('fill', 0, 0, 800, y_Coord) 
+	 	end
+	 	if playcolR == 1 then
+	    	love.graphics.setColor(0, Pulse+150, Pulse+25, Pulse)
+	   		love.graphics.rectangle('fill', x_Coord+(Board_x*gridSize), 0, x_Coord, 600) 
+	 	end
+	 	if playcolD == 1 then
+	    	love.graphics.setColor(0, Pulse+150, Pulse+25, Pulse)
+	   		love.graphics.rectangle('fill', 0, (y_Coord+Board_y*gridSize), 800, y_Coord) 
+	 	end
+	end
+
+
     function drawStart()
     	if startCnt < 1 then
 	    	for i,v in ipairs(Board) do
 	    		if v.startCnt == 1 then
-					love.graphics.draw(startimg, quad, v.x, v.y)
-		
-					love.graphics.setColor(0, 100, 50, Pulse)
+		    		love.graphics.draw(startimg, quad, v.x, v.y)
+
+					love.graphics.setColor(0, 100, 0, Pulse)
 	    			love.graphics.rectangle('fill', (playcolR-1+v.x/gridSize)*gridSize, v.y, 50, 50)
 
 					love.graphics.setColor(0, 255, 50, 50)
 	    			love.graphics.rectangle('fill', (playcolR-1+v.x/gridSize)*gridSize+4, v.y+4, 50-8, 50-8)
+	    			
+	    			love.graphics.setColor(0, 100, 0, Pulse)
+	    			love.graphics.rectangle('fill', v.x, (playcolD-1+v.y/gridSize)*gridSize, 50, 50)
 
-	    			love.graphics.setColor(0, 100, 50, Pulse)
+	    			love.graphics.setColor(0, 255, 50, 50)
+	    			love.graphics.rectangle('fill', v.x+4, (playcolD-1+v.y/gridSize)*gridSize+4, 50-8, 50-8)
+
+					love.graphics.setColor(0, 100, 0, Pulse)
+	    			love.graphics.rectangle('fill', (playcolL-1+v.x/gridSize)*gridSize, v.y, 50, 50)
+
+	    			love.graphics.setColor(0, 255, 50, 50)
+	    			love.graphics.rectangle('fill', (playcolL-1+v.x/gridSize)*gridSize+4, v.y+4, 50-8, 50-8)
+
+					love.graphics.setColor(0, 100, 0, Pulse)
 	    			love.graphics.rectangle('fill', v.x, (playcolU-1+v.y/gridSize)*gridSize, 50, 50)
 
 	    			love.graphics.setColor(0, 255, 50, 50)
 	    			love.graphics.rectangle('fill', v.x+4, (playcolU-1+v.y/gridSize)*gridSize+4, 50-8, 50-8)
-
-	    			--[[love.graphics.setColor(255, 255, 255, 50)
-	    			love.graphics.rectangle('fill', (playcolL-1+v.x/gridSize)*gridSize+4, v.y+4, 50-8, 50-8)
-
-	    			love.graphics.setColor(255, 255, 255, 50)
-	    			love.graphics.rectangle('fill', v.x+4, (playcolD+1+v.y/gridSize)*gridSize+4, 50-8, 50-8)]]
 				end
 			end
 		end
 	end
 	drawStart()
+
 end
 
 
